5869079a26863ace9891bad4e62b39cbf6dd2241
Jane67 <3088494894@qq.com>
2024-09-17 18:29:34 +0800

    recommend use

--- packages/Message/Message.tsx
+++ packages/Message/Message.tsx
@@ -7,8 +7,6 @@ export interface IMessage {
 }
 
 const Message = (prop: IMessage) => {
-  console.log('孟静娴她太厉害了', prop.type, prop.content);
-
   return (
     <div className={`j-message j-message-${prop.type}`}>{prop.content}</div>
   );
--- packages/Message/MessageManager.tsx
+++ packages/Message/MessageManager.tsx
@@ -19,22 +19,10 @@ const MessageManager = () => {
         );
       }, 3000);
     };
-    console.log('问渠哪得清如许');
   }, []);
 
   return (
     <div className="message-container">
-      <Message type="info" content="这是一条提示信息"></Message>
-      <Message type="success" content="这是一条成功信息"></Message>
-      <Message
-        type="warn"
-        content="弹性盒子由弹性容器(Flex container)和弹性子元素(Flex item)组成。弹性容器通过设置 display 属性的值为 flex 或 inline-flex将其定义为弹性容器。弹性容器内包含了一个或多个弹性子元素。"
-      ></Message>
-      <Message
-        type="error"
-        content="--------------------------我爱你但是我觉得配不上你-------------------------------"
-      ></Message>
-
       {messages.map((item, index) => (
         <Message key={index} type={item.type} content={item.content}></Message>
       ))}
@@ -43,8 +31,6 @@ const MessageManager = () => {
 };
 
 export const addMessage = (message: IMessage) => {
-  console.log('这儿没过来吗？');
-
   if (addMessageHandler) {
     addMessageHandler(message);
   }
